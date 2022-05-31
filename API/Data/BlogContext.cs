b2f71e7d1efb2f3cb88cb2ad4b3dc4271cf28e3f
KIM HANNA <kimhanna.contact@gmail.com>
2022-05-31 22:12:21 +0900

    DbContext, connection string added

---
+++ API/Data/BlogContext.cs
@@ -0,0 +1,13 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class BlogContext : DbContext
+    {
+        public BlogContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
---
+++ API/Entities/Post.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Entities
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int AuthorId { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string Text { get; set; }
+    }
+}
