8466bbaa76381008853194fc7253274c82a8578e
KIM HANNA <kimhanna.contact@gmail.com>
2022-06-06 23:12:51 +0900

    API controller to return a list of posts with async method

---
+++ API/Controllers/PostsController.cs
@@ -0,0 +1,31 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PostsController : ControllerBase
+    {
+        private readonly BlogContext _context;
+
+        public PostsController(BlogContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Post>>> GetPosts()
+        {
+            return await _context.Posts.ToListAsync();
+            
+        }
+        [HttpPost("{id}")]
+        public async Task<ActionResult<Post>> GetPost(int id)
+        {
+            return await _context.Posts.FindAsync(id);
+        }
+    }
+}
