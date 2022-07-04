400116ed8e09c1c553b1e59b891e28b31623f455
KIM HANNA <kimhanna.contact@gmail.com>
2022-07-04 21:35:35 +0900

    blog menu and stack added

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,13 +1,103 @@
-import { AppBar, Toolbar, Typography } from "@mui/material"
+import { Check } from "@material-ui/icons";
+import { AppBar, Button, Divider, Grid, InputBase, ListItemIcon, ListItemText, MenuItem, MenuList, Paper, Toolbar, Typography } from "@mui/material";
+import { ThemeProvider, createTheme, styled, alpha } from '@mui/material/styles';
+import './styles.css'
+
+  const titletheme = createTheme({
+    typography: {
+      allVariants: {
+        fontFamily: 'Noto Sans',
+        textTransform: 'none',
+        fontSize: 80,
+      },
+    },
+    
+  }  
+  );
+
+
+  const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+      width: 'auto',
+    },
+  }));
+
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+      padding: theme.spacing(0, 0, 0, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        width: '12ch',
+        '&:focus': {
+          width: '15ch',
+        },
+      },
+    },
+  }));  
+
+  const menutheme = createTheme({
+    typography: {
+      allVariants: {
+        fontFamily: 'Noto Sans',
+        textTransform: 'none',
+        fontSize: 25,
+        
+      },
+      
+    },
+  });
+  
 
 export default function Header() {
     return (
-        <AppBar position='static'>
-            <Toolbar>
-                <Typography variant='h6'>
-                    HANNA'S BLOG
-                </Typography>
-            </Toolbar> 
-        </AppBar>
+        <>
+        
+        <ThemeProvider theme={titletheme}>
+        <Typography variant='h2' align= 'center'>
+        <Button color="inherit">HANNA'S BLOG</Button>
+        </Typography>
+        </ThemeProvider>
+        
+        <ThemeProvider theme={menutheme}>
+        <Paper sx={{ width: 320 }}>
+      <MenuList dense>
+        <MenuItem>
+          <ListItemText inset>DSA PRACTICE</ListItemText>
+        </MenuItem>
+        <MenuItem>
+          <ListItemText inset>COMPUTER SCIENCE</ListItemText>
+        </MenuItem>
+        <MenuItem>
+          <ListItemText inset>ABOUT</ListItemText>
+        </MenuItem>
+        <Divider />
+        <MenuItem>
+        <Search>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+        </MenuItem>
+        </MenuList>
+    </Paper>
+        
+        </ThemeProvider>
+        
+        </>
     )
 }
--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,4 +1,4 @@
-import { Box, Divider, List, ListItem, ListItemAvatar, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
+import { Box, Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Stack, Typography } from "@mui/material";
 import { Fragment } from "react"
 import { Post } from "../../app/models/post";
 interface Props {
@@ -8,56 +8,15 @@ interface Props {
 
 export default function Catalog({posts, addPost}: Props) { //destructuring
     return( // <></> is equivalent to <Fragment></Fragment>
-       <>      
-        
-
-    <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-      <nav aria-label="main mailbox folders">
-        <List>
-        <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                
-              </ListItemIcon>
-              <ListItemText primary="Top Posts" />
-            </ListItemButton>
-          </ListItem>
-
-        </List>
-      </nav>
-      <Divider />
-      <nav aria-label="secondary mailbox folders">
-        <List>
-        <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-              </ListItemIcon>
-              <ListItemText primary="Explore DSA" />
-              </ListItemButton>
-          </ListItem>
-
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>   
-              </ListItemIcon>
-              <ListItemText primary="CS Knowledge" />
-            </ListItemButton>
-          </ListItem>
-
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-              </ListItemIcon>
-              <ListItemText primary="Interview Tips" />
-            </ListItemButton>
-          </ListItem>
-        
-        </List>
-      </nav>
-    </Box>
-
-
-
+       <> 
+      <Box sx={{ width: '100%', maxWidth: 600, bgcolor: 'background.paper', }}>
+      <Stack spacing={0.5}>
+        <ListItem>Find Learning Materials</ListItem>
+        <ListItemButton>.NET</ListItemButton>
+        <ListItemButton>JAVA SPRING</ListItemButton>
+        <ListItemButton>FLUTTER</ListItemButton>
+      </Stack>     
+      </Box>  
     <List>
         {posts.map(post  => (
           <ListItem key={post.id}>
@@ -67,7 +26,7 @@ export default function Catalog({posts, addPost}: Props) { //destructuring
           </ListItem>
         ))}
         </List>
-
+      <button onClick={addPost}>New Post</button>
         </>
         
         
